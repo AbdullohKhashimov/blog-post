8a88f712de91450b04800625c1ea9331c7120429
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-10-24 08:45:14 +0900

    feat: create project related components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+import { Route, Switch, useHistory } from "react-router-dom";
+import Header from "./reusables/Header";
+import Nav from "./reusables/Nav";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./components/About";
+import Missing from "./components/Missing";
+import Footer from "./reusables/Footer";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Switch>
+        <Route path="/post">
+          <NewPost />
+        </Route>
+
+        <Route path="/post/:id">
+          <PostPage />
+        </Route>
+
+        <Route path="/about">
+          <About />
+        </Route>
+
+        <Route path="*">
+          <Missing />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/PostPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PostPage = () => {
+  return (
+    <div>
+      <h2>Post Page</h2>
+    </div>
+  );
+};
+
+export default PostPage;
