6994cb61405715d4c2e14cfba3e5319a43066e49
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-10-24 10:20:36 +0900

    feat: develop about and footer components

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,13 @@ import React from "react";
 
 const About = () => {
   return (
-    <main>
+    <main className="About">
       <h1>About</h1>
+      <p style={{ marginTop: "1rem" }}>
+        This is a blog post built on React.js where you can post or read through
+        posts list. Please feel free to share your thougts, feelings or
+        knowledge that you want to share with people through this posting app.
+      </p>
     </main>
   );
 };
--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Missing = () => {
   return (
-    <div>
-      <h1>Missing</h1>
-    </div>
+    <main className="Missing">
+      <h1>Page Not Found</h1>
+      <p>
+        <Link to="/">Go back homepage</Link>
+      </p>
+    </main>
   );
 };
 
