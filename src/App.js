960a7ba27f9afbabfae547b4179d544c421b4630
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-10-24 09:17:43 +0900

    feat: create some hard coded posts to display in homepage

--- src/App.js
+++ src/App.js
@@ -7,15 +7,45 @@ import PostPage from "./components/PostPage";
 import About from "./components/About";
 import Missing from "./components/Missing";
 import Footer from "./reusables/Footer";
+import { useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My first Post",
+      datetime: "June 02, 2024 11:20:35 AM",
+      body: "Lorem Ipsum dolor sit amet consectetur adispicing elit.",
+    },
+    {
+      id: 2,
+      title: "My first Post",
+      datetime: "June 04, 2024 11:20:35 AM",
+      body: "Lorem Ipsum dolor sit amet consectetur adispicing elit.",
+    },
+    {
+      id: 3,
+      title: "My first Post",
+      datetime: "June 20, 2024 11:20:35 AM",
+      body: "Lorem Ipsum dolor sit amet consectetur adispicing elit.",
+    },
+    {
+      id: 4,
+      title: "My first Post",
+      datetime: "June 30, 2024 11:20:35 AM",
+      body: "Lorem Ipsum dolor sit amet consectetur adispicing elit.",
+    },
+  ]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResult] = useState([]);
+
   return (
     <div className="App">
       <Header title="React JS Blog" />
-      <Nav />
+      <Nav search={search} setSearch={setSearch} />
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Home posts={posts} />
         </Route>
         <Route exact path="/post">
           <NewPost />
@@ -25,13 +55,9 @@ function App() {
           <PostPage />
         </Route>
 
-        <Route path="/about">
-          <About />
-        </Route>
+        <Route path="/about" component={About}></Route>
 
-        <Route path="*">
-          <Missing />
-        </Route>
+        <Route path="*" component={Missing}></Route>
       </Switch>
       <Footer />
     </div>
